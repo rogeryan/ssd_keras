45c364a122d2aa894ce8ba687876b02223a83cd0
Roger Mobile <2749234+rogeryan@users.noreply.github.com>
2020-01-24 10:58:53 +0800

    添加数据集前缀变量，方便适应不同环境；添加一个预测程序

--- ssd7_pred.py
+++ ssd7_pred.py
@@ -50,7 +50,6 @@ def init_log_config():
 
 
 init_log_config()
-
 # 1. Set the model configuration parameters
 img_height = 300  # Height of the input images
 img_width = 480  # Width of the input images
@@ -76,7 +75,7 @@ variances = [1.0, 1.0, 1.0, 1.0]
 normalize_coords = True
 dataset_prefix = './'
 model_saved = 'ssd7.h5'
-
+batch_size = 16
 
 # 加载之前保存的模型或者创建新模型
 K.clear_session()  # Clear previous models from memory.
@@ -86,64 +85,14 @@ if os.path.exists(model_saved):
     model = load_model(model_saved, custom_objects={
                        'AnchorBoxes': AnchorBoxes, 'compute_loss': ssd_loss.compute_loss})
 else:
-    model = build_model(image_size=(img_height, img_width, img_channels),
-                        n_classes=n_classes,
-                        mode='training',
-                        l2_regularization=0.0005,
-                        scales=scales,
-                        aspect_ratios_global=aspect_ratios,
-                        aspect_ratios_per_layer=None,
-                        two_boxes_for_ar1=two_boxes_for_ar1,
-                        steps=steps,
-                        offsets=offsets,
-                        clip_boxes=clip_boxes,
-                        variances=variances,
-                        normalize_coords=normalize_coords,
-                        subtract_mean=intensity_mean,
-                        divide_by_stddev=intensity_range)
+    logger.error(f'无法加载训练好的模型:{model_saved}')
 
-    # 2: Optional: Load some weights
-    #model.load_weights('./ssd7_weights.h5', by_name=True)
-    # 3: Instantiate an Adam optimizer and the SSD loss function and compile the model
-    adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
-    ssd_loss = SSDLoss(neg_pos_ratio=3, alpha=1.0)
-    model.compile(optimizer=adam, loss=ssd_loss.compute_loss)
-
-
-train_dataset = DataGenerator(
-    load_images_into_memory=False, hdf5_dataset_path=dataset_prefix+'traffic_train.h5')
 val_dataset = DataGenerator(
     load_images_into_memory=False, hdf5_dataset_path=dataset_prefix+'traffic_val.h5')
-train_dataset_size = train_dataset.get_dataset_size()
 val_dataset_size = val_dataset.get_dataset_size()
-
-logger.info("Number of images in the training dataset:\t{:>6}".format(
-    train_dataset_size))
 logger.info("Number of images in the validation dataset:\t{:>6}".format(
     val_dataset_size))
 
-
-# part 3
-# 3: Set the batch size.
-batch_size = 16
-# 4: Define the image processing chain.
-data_augmentation_chain = DataAugmentationConstantInputSize(
-    random_brightness=(-48, 48, 0.5),
-    random_contrast=(0.5, 1.8, 0.5),
-    random_saturation=(0.5, 1.8, 0.5),
-    random_hue=(18, 0.5),
-    random_flip=0.5,
-    random_translate=((0.03, 0.5), (0.03, 0.5), 0.5),
-    random_scale=(0.5, 2.0, 0.5),
-    n_trials_max=3,
-    clip_boxes=True,
-    overlap_criterion='area',
-    bounds_box_filter=(0.3, 1.0),
-    bounds_validator=(0.5, 1.0),
-    n_boxes_min=1,
-    background=(0, 0, 0))
-# 5: Instantiate an encoder that can encode ground truth labels into the format needed by the SSD loss function.
-# The encoder constructor needs the spatial dimensions of the model's predictor layers to create the anchor boxes.
 predictor_sizes = [model.get_layer('classes4').output_shape[1:3],
                    model.get_layer('classes5').output_shape[1:3],
                    model.get_layer('classes6').output_shape[1:3],
@@ -165,15 +114,6 @@ ssd_input_encoder = SSDInputEncoder(img_height=img_height,
                                     neg_iou_limit=0.3,
                                     normalize_coords=normalize_coords)
 
-# 6: Create the generator handles that will be passed to Keras' `fit_generator()` function.
-train_generator = train_dataset.generate(batch_size=batch_size,
-                                         shuffle=True,
-                                         transformations=[data_augmentation_chain],
-                                         label_encoder=ssd_input_encoder,
-                                         returns={'processed_images',
-                                                  'encoded_labels'},
-                                         keep_images_without_gt=False)
-
 val_generator = val_dataset.generate(batch_size=batch_size,
                                      shuffle=False,
                                      transformations=[],
@@ -182,63 +122,67 @@ val_generator = val_dataset.generate(batch_size=batch_size,
                                               'encoded_labels'},
                                      keep_images_without_gt=False)
 
-# part 4
-# Define model callbacks.
-# TODO: Set the filepath under which you want to save the weights.
-model_checkpoint = ModelCheckpoint(filepath='ssd7_epoch-{epoch:02d}_loss-{loss:.4f}_val_loss-{val_loss:.4f}.h5',
-                                   monitor='val_loss',
-                                   verbose=1,
-                                   save_best_only=True,
-                                   save_weights_only=False,
-                                   mode='auto',
-                                   period=1)
+# Make predication
+predict_generator = val_dataset.generate(batch_size=1,
+                                         shuffle=True,
+                                         transformations=[],
+                                         label_encoder=None,
+                                         returns={'processed_images',
+                                                  'processed_labels',
+                                                  'filenames'},
+                                         keep_images_without_gt=False)
 
-csv_logger = CSVLogger(filename='ssd7_training_log.csv',
-                       separator=',',
-                       append=True)
+# 2: Generate samples
+batch_images, batch_labels, batch_filenames = next(predict_generator)
+i = 0  # Which batch item to look at
+logger.info("Image:" + batch_filenames[i])
+logger.info("Ground truth boxes:")
+logger.info(batch_labels[i])
 
-early_stopping = EarlyStopping(monitor='val_loss',
-                               min_delta=0.0,
-                               patience=10,
-                               verbose=1)
+y_pred = model.predict(batch_images)
+y_pred_decoded = decode_detections(y_pred,
+                                   confidence_thresh=0.5,
+                                   iou_threshold=0.45,
+                                   top_k=200,
+                                   normalize_coords=normalize_coords,
+                                   img_height=img_height,
+                                   img_width=img_width)
 
-reduce_learning_rate = ReduceLROnPlateau(monitor='val_loss',
-                                         factor=0.2,
-                                         patience=8,
-                                         verbose=1,
-                                         epsilon=0.001,
-                                         cooldown=0,
-                                         min_lr=0.00001)
+np.set_printoptions(precision=2, suppress=True, linewidth=90)
+logger.info("Predicted boxes:\n")
+logger.info('   class   conf xmin   ymin   xmax   ymax')
+logger.info(y_pred_decoded[i])
 
-callbacks = [model_checkpoint,
-             csv_logger,
-             early_stopping,
-             reduce_learning_rate]
+plt.figure(figsize=(20, 12))
+plt.imshow(batch_images[i])
 
-# TODO: Set the epochs to train for.
-# If you're resuming a previous training, set `initial_epoch` and `final_epoch` accordingly.
-initial_epoch = 0
-final_epoch = 20
-steps_per_epoch = 1000
+current_axis = plt.gca()
 
-## test
-tr = iter(train_generator)
-tr0 = next(tr)
-print(type(tr0))
-for x in tr0:
-    logger.info(x.shape)
-    logger.info('='*30)
+# Set the colors for the bounding boxes
+colors = plt.cm.hsv(np.linspace(0, 1, n_classes+1)).tolist()
+# Just so we can print class names onto the image instead of IDs
+classes = ['background', 'car', 'truck', 'pedestrian', 'bicyclist', 'light']
 
+# Draw the ground truth boxes in green (omit the label for more clarity)
+for box in batch_labels[i]:
+    xmin = box[1]
+    ymin = box[2]
+    xmax = box[3]
+    ymax = box[4]
+    label = '{}'.format(classes[int(box[0])])
+    current_axis.add_patch(plt.Rectangle(
+        (xmin, ymin), xmax-xmin, ymax-ymin, color='green', fill=False, linewidth=2))
+    #current_axis.text(xmin, ymin, label, size='x-large', color='white', bbox={'facecolor':'green', 'alpha':1.0})
 
-history = model.fit_generator(generator=train_generator,
-                    steps_per_epoch=steps_per_epoch,
-                    epochs=final_epoch,
-                    callbacks=callbacks,
-                    validation_data=val_generator,
-                    validation_steps=ceil(val_dataset_size/batch_size),
-                    initial_epoch=initial_epoch)
-
-plt.figure(figsize=(20,12))
-plt.plot(history.history['loss'], label='loss')
-plt.plot(history.history['val_loss'], label='val_loss')
-plt.legend(loc='upper right', prop={'size': 24})
+# Draw the predicted boxes in blue
+for box in y_pred_decoded[i]:
+    xmin = box[-4]
+    ymin = box[-3]
+    xmax = box[-2]
+    ymax = box[-1]
+    color = colors[int(box[0])]
+    label = '{}: {:.2f}'.format(classes[int(box[0])], box[1])
+    current_axis.add_patch(plt.Rectangle(
+        (xmin, ymin), xmax-xmin, ymax-ymin, color=color, fill=False, linewidth=2))
+    current_axis.text(xmin, ymin, label, size='x-large',
+                      color='white', bbox={'facecolor': color, 'alpha': 1.0})
